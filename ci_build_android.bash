cea86b4d73a2aba47b1d6a5b09d47dfa9778376c
Lunarixus <Nathan@lunarixus.dev>
2024-05-08 03:03:34 +0100

    ci_build_android: Ensure we definitely export offline build
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

--- ci_build_android.bash
+++ ci_build_android.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 export LIBFFI_VERSION=3.4.2
-export ANDROID=1 LWJGL_BUILD_OFFLINE=1
+export ANDROID=1
+export LWJGL_BUILD_OFFLINE=1
 #export LWJGL_BUILD_ARCH=arm64
 
 # Setup env
