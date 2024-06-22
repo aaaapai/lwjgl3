267d19fb8dbc3e3ca51a9dfed29995e5af168719
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-06-22 19:21:42 +0800

    Update ci_build_android.bash

--- ci_build_android.bash
+++ ci_build_android.bash
@@ -49,10 +49,6 @@ wget -nc $POJAV_NATIVES/libopenal.so -P $LWJGL_NATIVE/openal
 wget -nc "https://nightly.link/aaaapai/shaderc/workflows/android/main/libshaderc-$NDK_ABI.zip"
 unzip -o libshaderc-$NDK_ABI.zip -d $LWJGL_NATIVE/shaderc
 
-# HACK: Skip compiling and running the generator to save time and keep LWJGLX functions
-mkdir -p bin/classes/{generator,templates/META-INF}
-touch bin/classes/{generator,templates}/touch.txt bin/classes/generator/generated-touch.txt
-
 # Build LWJGL 3
 ant -version
 yes | ant -Dplatform.linux=true \
@@ -89,7 +85,7 @@ yes | ant -Dplatform.linux=true \
   -Dbuild.type=nightly/3.3.4 \
   -Djavadoc.skip=true \
   -Dnashorn.args="--no-deprecation-warning" \
-  compile compile-native release
+  compile-templates compile compile-native release
 
 # Copy native libraries
 rm -rf bin/out; mkdir bin/out
