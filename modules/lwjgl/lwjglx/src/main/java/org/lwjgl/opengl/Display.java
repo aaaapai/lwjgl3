85def5c0008552f14b23207dace0f453b0c0b2ec
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-08-11 11:14:38 +0800

    Update Display.java

--- modules/lwjgl/lwjglx/src/main/java/org/lwjgl/opengl/Display.java
+++ modules/lwjgl/lwjglx/src/main/java/org/lwjgl/opengl/Display.java
@@ -82,8 +82,7 @@ public class Display {
 
         mode = desktopDisplayMode = new DisplayMode(monitorWidth, monitorHeight, monitorBitPerPixel, monitorRefreshRate);
         LWJGLUtil.log("Initial mode: " + desktopDisplayMode);
-        if("opengles2".equals(System.getenv("POJAV_BETA_RENDERER"))
-        || "opengles2_vgpu".equals(System.getenv("POJAV_BETA_RENDERER"))) GLContext.initCapabilities();
+        if("opengles2".equals(System.getenv("POJAV_BETA_RENDERER"))) GLContext.initCapabilities();
     }
     
     public static void setSwapInterval(int value) {
