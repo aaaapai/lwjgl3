536177796b45d4154f97f493d441a11d5a22f855
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-07-26 20:48:40 +0800

    Update Display.java

--- modules/lwjgl/lwjglx/src/main/java/org/lwjgl/opengl/Display.java
+++ modules/lwjgl/lwjglx/src/main/java/org/lwjgl/opengl/Display.java
@@ -82,7 +82,7 @@ public class Display {
 
         mode = desktopDisplayMode = new DisplayMode(monitorWidth, monitorHeight, monitorBitPerPixel, monitorRefreshRate);
         LWJGLUtil.log("Initial mode: " + desktopDisplayMode);
-        if("opengles2".equals(System.getenv("POJAV_RENDERER"))) GLContext.initCapabilities();
+        if("ooooooopengles114514".equals(System.getenv("POJAV_RENDERER"))) GLContext.initCapabilities();
     }
     
     public static void setSwapInterval(int value) {
