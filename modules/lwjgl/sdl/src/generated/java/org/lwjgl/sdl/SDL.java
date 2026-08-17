fb5e07e558ef414b4582e2e354352ee1dc407b7d
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-08-17 18:38:19 +0800

    Update SDL.java

--- modules/lwjgl/sdl/src/generated/java/org/lwjgl/sdl/SDL.java
+++ modules/lwjgl/sdl/src/generated/java/org/lwjgl/sdl/SDL.java
@@ -1,17 +1,36 @@
-/*
- * Copyright LWJGL. All rights reserved.
- * License terms: https://www.lwjgl.org/license
- * MACHINE GENERATED FILE, DO NOT EDIT
- */
 package org.lwjgl.sdl;
 
 import org.lwjgl.system.*;
 
 public final class SDL {
 
-    private static final SharedLibrary SDL = Library.loadNative(SDL.class, "org.lwjgl.sdl", Configuration.SDL_LIBRARY_NAME.get(Platform.mapLibraryNameBundled("SDL3")), true);
+    private static final SharedLibrary SDL;
 
-    /** Returns the sdl {@link SharedLibrary}. */
+    // Native method to load SDL library via dlopen
+    private static native long nLoadSDL(String libName);
+
+    static {
+        String specifiedLib = Configuration.SDL_LIBRARY_NAME.get();
+        SharedLibrary loaded = null;
+
+        if (specifiedLib != null) {
+            long handle = nLoadSDL(specifiedLib);
+            if (handle != 0) {
+                loaded = Library.createFromHandle(specifiedLib, handle);
+                apiLog("[SDL] Loaded specified library: " + specifiedLib + " via native dlopen.");
+            } else {
+                apiLog("[SDL] Failed to load specified library: " + specifiedLib + ", falling back to default.");
+            }
+        }
+
+        if (loaded == null) {
+            loaded = Library.loadNative(SDL.class, "org.lwjgl.sdl", Configuration.SDL_LIBRARY_NAME.get(Platform.mapLibraryNameBundled("SDL3")), true);
+        }
+
+        SDL = loaded;
+    }
+
+    /** Returns the SDL {@link SharedLibrary}. */
     public static SharedLibrary getLibrary() {
         return SDL;
     }
@@ -20,4 +39,8 @@ public final class SDL {
         throw new UnsupportedOperationException();
     }
 
+    private static void apiLog(String msg) {
+        System.out.println(msg);
+    }
 }
+
